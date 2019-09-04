c5d18a781abd66ced5f658c385388f391e6421f6
Claudiu Mera <claudiu.mera@live.com>
2019-09-04 08:29:12 +0300

    Added Singleton project

---
+++ DesignPatternsTraining/Singleton/LazyLoader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class LazyLoader
+    {
+        private static readonly Lazy<LazyLoader> lazy = new Lazy<LazyLoader>(() => new LazyLoader());
+
+        public static LazyLoader Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+
+        private LazyLoader()
+        {
+
+        }
+    }
+}
---
+++ DesignPatternsTraining/Singleton/Loader.cs
@@ -0,0 +1,23 @@
+﻿namespace Singleton
+{
+    public class Loader
+    {
+        private static Loader _instance;
+
+        public static Loader Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new Loader();
+                }
+
+                return _instance;
+            }
+        }
+        private Loader()
+        {
+        }
+    }
+}
