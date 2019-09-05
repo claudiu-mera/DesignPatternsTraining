05de9afa35d4f88971527bba1e138cbf07cc3795
Claudiu MERA <Claudiu.MERA@endava.com>
2019-09-05 11:59:31 +0300

    Updated Singleton implementation

--- DesignPatternsTraining/Singleton/Program.cs
+++ DesignPatternsTraining/Singleton/Program.cs
@@ -6,7 +6,17 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Loader firstInstance = Loader.Instance;
+            Loader secondInstance = Loader.Instance;
+
+            Console.WriteLine($"First instance equals second instance {firstInstance.Equals(secondInstance)}");
+
+            LazyLoader firstLazyInstance = LazyLoader.Instance;
+            LazyLoader secondLazyInstance = LazyLoader.Instance;
+
+            Console.WriteLine($"First lazy instance equals second lazy instance {firstInstance.Equals(secondInstance)}");
+
+            Console.ReadKey();
         }
     }
 }
